203af504540b524f7850c3c3e0bcc23f3b7e7778
Seth R Johnson <johnsonsr@ornl.gov>
2026-09-08 16:10:45 -0400

    Use try-emplace rather than emplace

--- src/modules/TransientPropagation/TransientPropagationModule.cpp
+++ src/modules/TransientPropagation/TransientPropagationModule.cpp
@@ -835,11 +835,9 @@ TransientPropagationModule::propagate(Event* event,
                        << " q = " << Units::display(induced, "e");
 
             // Create pulse if it doesn't exist. Store induced charge in the returned pulse iterator
-            auto pixel_map_iterator = pixel_map.emplace(std::piecewise_construct,
-                                                        std::forward_as_tuple(pixel_index),
-                                                        std::forward_as_tuple(timestep_, integration_time_));
+            auto&& [iter, emplaced] = pixel_map.try_emplace(pixel_index, timestep_, integration_time_);
             try {
-                pixel_map_iterator.first->second.addCharge(induced, initial_time_local + runge_kutta.getTime());
+                iter->second.addCharge(induced, initial_time_local + runge_kutta.getTime());
             } catch(const PulseBadAllocException& e) {
                 LOG(ERROR) << e.what() << '\n'
                            << "Ignoring pulse contribution at time "
